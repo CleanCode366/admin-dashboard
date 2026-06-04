1df87568ca71dff8bca44b3cba3e096eff88a857
myusername <sarthaksam003@gmail.com>
2026-06-04 12:06:06 +0530

    Feat: Added the Logout functionality and fixed the filter by status in the SidebarNav

--- src/shared/composites/AvatarMenu/AvatarMenu.tsx
+++ src/shared/composites/AvatarMenu/AvatarMenu.tsx
@@ -15,7 +15,6 @@ import {
 import { useTheme } from '@/shared/theme'
 import { Button } from '@/shared/primitives/Button'
 import { useToast } from '@/hooks/useToast'
-import { NavLink } from 'react-router-dom'
 
 export interface AvatarMenuProps {
   name: string
@@ -28,17 +27,17 @@ export interface AvatarMenuProps {
 
   onSecurity?: () => void
 
-  onLogout?: () => void
+  onLogout?: () => void | Promise<void | boolean>
 }
 
 export function AvatarMenu({
   name,
   src,
 
-  onEditProfile = () => { },
-  onPreferences = () => { },
-  onSecurity = () => { },
-  onLogout = () => { },
+  onEditProfile = () => {},
+  onPreferences = () => {},
+  onSecurity = () => {},
+  onLogout = () => {},
 }: AvatarMenuProps) {
   const [open, setOpen] = useState(false)
   const { themeName, setTheme } = useTheme()
@@ -135,20 +134,21 @@ export function AvatarMenu({
               size="sm"
               className="ml-auto"
               checked={themeName === 'dark'}
-              onChange={() => { }}
+              onChange={() => {}}
             />
           </Button>
           <div className="bg-border-tertiary my-1 h-px" />
-          <NavLink to='/login'>
-            <Button
-              onClick={onLogout}
-              className="contents-center text-text-danger hover:bg-bg-danger flex w-full cursor-pointer flex-row items-center gap-2 rounded-md px-3 py-2 text-left text-sm transition"
-              variant={'danger'}
-              leftIcon={<ArrowRightStartOnRectangleIcon className="h-5 w-5" />}
-            >
-              <div>Logout</div>
-            </Button>
-          </NavLink>
+          <Button
+            onClick={async () => {
+              setOpen(false)
+              await onLogout()
+            }}
+            className="contents-center text-text-danger hover:bg-bg-danger flex w-full cursor-pointer flex-row items-center gap-2 rounded-md px-3 py-2 text-left text-sm transition"
+            variant={'danger'}
+            leftIcon={<ArrowRightStartOnRectangleIcon className="h-5 w-5" />}
+          >
+            <div>Logout</div>
+          </Button>
         </div>
       )}
     </div>
